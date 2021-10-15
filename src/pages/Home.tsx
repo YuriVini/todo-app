6646d90930bc2a8a18852f1a3c0131243c8505a5
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2021-10-15 11:22:09 -0300

    First challenger of Ignite

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task}))
+    const foundTask = updatedTasks.find(item => (item.id === id))
+    
+    if(!foundTask)
+      return;
+    
+    foundTask.done = !foundTask.done;
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+  ))
   }
 
   return (
